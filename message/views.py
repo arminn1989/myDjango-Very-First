e046ba250e450bd209038e32b1d5eab0d3111a77
Sara <sara@192.168.1.104>
2024-01-26 17:37:32 +0000

    message app is done

---
+++ message/views.py
@@ -0,0 +1,10 @@
+from django.views.generic import TemplateView
+
+
+
+
+# def messageView(request):
+#     return render(request, 'home.html')
+
+class MessageView(TemplateView):
+    template_name = 'home.html'
